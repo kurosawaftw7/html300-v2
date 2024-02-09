125fef0d5ffb1cf34480f4092e4376a4410fbcdc
kurosawaftw7 <jdharman@gmail.com>
2024-02-09 13:06:40 -0800

    added array, template and script code, contains errors.

---
+++ lesson08/activity/starter/component-activity/src/post.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+    <article class="post">
+        <h3 class="post_title">{{ post.title }}</h3>
+        <h6 class="post_author">{{ post.author }}</h6>
+        <p class="post_excerpt">{{ post.excerpt }}</p>
+        <button @click="post.likes++" class="post_likes">+1 | {{ post.likes }}</button>
+    </article>
+    </div>
+</template>
+  
+<script>
+    export default {
+        name: 'post',
+        props: ['posts'],
+    }
+</script>
